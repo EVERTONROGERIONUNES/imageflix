815f0383fe5b48d60ccdb74105add2b932bcceca
EVERTONROGERIONUNES <70280660+EVERTONROGERIONUNES@users.noreply.github.com>
2023-05-16 21:32:38 -0300

    aula 23 adding carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css';
-
 import Hero from './components/Hero';
-
+import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
+import './App.css';
+import movies from './mock/movies.mock';
 
 function App() {
   return (
     <>
-      <Hero />
+      <Hero score={10} />
+      <Navbar />
+      <Carousel title='Filmes' data={movies} />
     </>
   )
 }
