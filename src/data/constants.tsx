800370e332d8d02bb77a0a4c8d1f50867e2ff8a4
EVERTONROGERIONUNES <70280660+EVERTONROGERIONUNES@users.noreply.github.com>
2023-05-26 07:38:32 -0300

    API

---
+++ src/data/constants.tsx
@@ -0,0 +1,11 @@
+const URL = 'https://api.themoviedb.org/3';
+const APIKEY = '0d1aa6e5a33f17497e9b24fc7382958c';
+const IMAGE_URL = 'https://image.tmdb.org/t/p';
+const APISTRING = `?api_key=${APIKEY}&language=pt-BR`;
+
+export default {
+  URL,
+  APIKEY,
+  IMAGE_URL,
+  APISTRING
+}
